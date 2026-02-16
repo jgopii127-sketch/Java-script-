cd1195c8b33859be222ec42dbdd98ea67968fdba
jgopii127-sketch <jgopii127@gmail.com>
2026-02-16 16:13:58 +0530

    Add files via upload

---
+++ methods.js
@@ -0,0 +1,109 @@
+// 11/02/2026:
+
+// STRING METHODS PRACTICE
+
+// let name = "  Jeya Gopi  ";
+
+// console.log("Original:", name);
+
+// // 1. length
+// console.log("Length:", name.length);
+
+// // 2. trim()
+// let trimmed = name.trim();
+// console.log("Trimmed:", trimmed);
+
+// // 3. toUpperCase()
+// console.log("Uppercase:", trimmed.toUpperCase());
+
+// // 4. toLowerCase()
+// console.log("Lowercase:", trimmed.toLowerCase());
+
+// // 5. includes()
+// console.log("Includes 'Gopi':", trimmed.includes("Gopi"));
+
+// // 6. startsWith()
+// console.log("Starts with 'Je':", trimmed.startsWith("Je"));
+
+// // 7. endsWith()
+// console.log("Ends with 'pi':", trimmed.endsWith("pi"));
+
+// // 8. slice()
+// console.log("Slice (0,4):", trimmed.slice(0, 4));
+
+// // 9. replace()
+// console.log("Replace:", trimmed.replace("Gopi", "Micca"));
+
+// // 10. split()
+// let words = trimmed.split(" ");
+// console.log("Split:", words);
+
+
+// ARRAY METHODS PRACTICE
+
+// let marks = [35, 78, 90, 32, 22];
+
+// console.log("Original:", marks);
+
+// // 1. length
+// console.log("Length:", marks.length);
+
+// // 2. push()
+// marks.push(100);
+// console.log("After push:", marks);
+
+// // 3. pop()
+// marks.pop();
+// console.log("After pop:", marks);
+
+// // 4. shift()
+// marks.shift();
+// console.log("After shift:", marks);
+
+// // 5. unshift()
+// marks.unshift(50);
+// console.log("After unshift:", marks);
+
+// // 6. includes()
+// console.log("Includes 90:", marks.includes(90));
+
+// // 7. indexOf()
+// console.log("Index of 78:", marks.indexOf(78));
+
+// // 8. slice()
+// console.log("Slice (1,3):", marks.slice(1, 3));
+
+// // 9. splice()
+// marks.splice(1, 1); // remove 1 element from index 1
+// console.log("After splice:", marks);
+
+// // 10. forEach()
+// marks.forEach((mark) => {
+//   console.log("Mark:", mark);
+// });
+
+
+// NUMBER METHODS PRACTICE
+
+// let num = 123.456789;
+
+// // toFixed
+// console.log("ToFixed (2):", num.toFixed(2));
+
+// // toPrecision
+// console.log("ToPrecision (4):", num.toPrecision(4));
+
+// // parseInt
+// console.log("ParseInt:", parseInt("100px"));
+
+// // parseFloat
+// console.log("ParseFloat:", parseFloat("10.99kg"));
+
+// // Number()
+// console.log("Number:", Number("123"));
+
+// // isNaN
+// console.log("isNaN:", isNaN("Hello"));
+
+// // typeof
+// console.log("Type of num:", typeof num);

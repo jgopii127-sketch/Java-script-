718352697581215f92d5058a1d3edbf91ee3c4d7
jgopii127-sketch <jgopii127@gmail.com>
2026-02-18 21:26:28 +0530

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,149 @@
+// 18/02/2026:
+
+// loop - its robot  it repests the task again and again
+
+// for (let a = 1; a < 5; a++) {
+//   console.log("hello", a);
+// }
+// for (let a = 10; a > 5; a--) {
+//   console.log("hello", a);
+// }
+// // console.log("hello");
+// //while loop
+// let i = 2;
+// while(i<= 8){
+//     console.log("hi",i)
+//     i++;
+// }
+
+// //do while - runs atleast once
+// let d = 1;
+// do{
+//     console.log("welcome", d)
+//     d++
+// }while(d>7)
+
+// //reverse
+
+// let str = "hellodrftgvybhunjimk";
+// let reversed = "";
+
+// for(let i =str.length - 1; i>=0; i--){
+//     reversed += str[i];  /// reversed = reversed +str[i]
+// }
+// console.log(str.length);
+// console.log(reversed)
+
+//if else....
+
+// let age = 20;
+
+// if (age >= 18) {
+//   console.log("you are eligible to vote");
+//   if (age > 200) {
+//     console.log("hi");
+//   } else {
+//     console.log("bye");
+//   }
+// } else {
+//   console.log("you are not eligible to vote");
+// }
+
+// let gopi = "gopi";
+// if (gopi == "male") {
+//   console.log("gopi is a male");
+// }
+// else if (gopi == "female") {
+//   console.log("gopi is a female");
+// }
+// else {
+//   console.log("else works");
+// }
+
+// ---------------------------------------------------------------------------
+
+// let marks=[12,13,14,15,16];
+
+// marks.forEach(function (mark) {
+//     console.log(mark);
+    
+// });
+
+// let names=["jeya","gopi","micca"];
+
+// names.forEach(function (NewName){
+//     console.log(NewName);
+// });
+
+// let str="javascript";
+// let reversed="";
+
+// for(let i = str.length-1; i>=0;i--){
+//     reversed +=str[i];
+// }
+// console.log(reversed);
+
+//---------------------------------------------------------------------------------- 
+
+// TASK IN LOOPS & IF ELSE CONCEPT:
+
+// // i.Write a program to print numbers from 1 to 10 using a for loop.
+
+// for (let i=1; i<=10;i++){
+//     console.log(i);
+// }
+
+// // ii.Write a program to print numbers from 10 to 1 (reverse order).
+
+// for (let i=10;i>=1;i--){
+//     console.log(i);
+// }
+
+// // iii.Write a program to print all even numbers between 1 and 50.
+
+// // using for loop with if condition:
+
+// for (let i=1;i<=50;i++){
+//     if (i % 2=== 0) {
+//         console.log("even number"+i);
+//     }
+// }
+
+// // using for loop only:
+
+// for (let i = 2; i <= 50; i += 2) {
+//   console.log(i);
+// }
+
+// // iv.Write a program to print all odd numbers between 1 and 50.
+
+// // using for loop with if condition:
+
+// for (let i = 1; i <= 50; i++){
+//   if (1 % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+
+// // using for loop only:
+
+// for (let i=1;i<=50;i+=2){
+//     console.log("odd number"+i);
+// }
+
+// // v.Write a program to check whether a number & character is a palindrome.
+
+// let num= "Never odd or even";
+
+// let org=num;
+// let res="";
+
+// for (let i=org.length - 1;i>=0;i--){
+//     res+=org[i];
+// }
+// if (org==res){
+//     console.log(org + " " + "is a Pallindrome");
+// }else{
+//     console.log(org + " " + "is Not a Pallindrom");
+// }
